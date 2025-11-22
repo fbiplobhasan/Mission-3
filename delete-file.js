aa8866058e25b683e17b67e974c03c315a140753
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-22 14:13:58 +0600

    add asynchronously deleted

--- delete-file.js
+++ delete-file.js
@@ -5,13 +5,30 @@ console.log("Temp file created.");
 
 if (fs.existsSync("./output/temp.txt")) {
     console.log("file exist!!!");
+
+    fs.unlinkSync("./output/temp.txt")
+    console.log("file deleted");
 }
 
-fs.unlinkSync("./output/temp.txt")
-console.log("file deleted");
+
 
 try {
-
+    fs.unlinkSync("./output/temp.txt");
 } catch (error) {
-    console.error(error.message);
+    console.log('ERROR', error.message);
 }
+
+console.log("Deleted");
+
+fs.writeFile("./output/temp2.txt", "Another file", (err) => {
+    if (err) return console.error(err.message);
+    console.log("Another temp file created.");
+
+    fs.unlink("./output/temp2.txt", (err) => {
+        if (err) {
+            console.error("Error", err.message);
+        } else {
+            console.log("Temp2 deleted");
+        }
+    })
+})
