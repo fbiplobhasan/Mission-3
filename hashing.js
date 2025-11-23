e7df63b37cab6869fa4ead88da91f89f58011c14
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-23 12:42:24 +0600

    hashing

---
+++ hashing.js
@@ -0,0 +1,16 @@
+const crypto = require("crypto");
+
+console.log("\n MD5 Hash: ");
+const md5Hash = crypto.createHash("md5").update("password123").digest("hex");
+const md5Hash2 = crypto.createHash("md5").update("password123").digest("hex");
+console.log("input : password123");
+console.log("MD5 HashPassword: ", md5Hash);
+console.log("MD5 Hash2: ", md5Hash2);
+
+const sha256Hash = crypto.createHash("sha256").update("password123").digest("hex")
+console.log("input : password123");
+console.log("sha256 HashPassword: ", sha256Hash);
+
+const sha512Hash = crypto.createHash("sha512").update("password123").digest("hex")
+console.log("input : password123");
+console.log("sha512 HashPassword: ", sha512Hash);
